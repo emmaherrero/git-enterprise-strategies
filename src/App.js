89291d2c51c3199cb18f1539c84e71841f4fc59f
samilieberman <samilieberman1@gmail.com>
2022-11-06 19:28:41 -0500

    feat: personalize greeting and prep v1.0.0 - Update App text to "My name is Emma Herrero" - Include prior helloWorld branch changes - Bump version to 1.0.0

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+<<<<<<< HEAD
 import gator from './gator.png';
+=======
+import gatorLogo from './gator.gif';
+>>>>>>> 2a3be3b (feat: personalize greeting and prep v1.0.0)
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+<<<<<<< HEAD
         <img src={gator} className="App-logo" alt="gator" />
         <p>
           Hello World! This is our team project.
         </p>	
+=======
+        <img src={gatorLogo} className="App-logo" alt="logo" />
+        <p> Hello World! </p>
+        <p> My name is Emma Herrero and this is my project </p>
+>>>>>>> 2a3be3b (feat: personalize greeting and prep v1.0.0)
       </header>
     </div>
   );
