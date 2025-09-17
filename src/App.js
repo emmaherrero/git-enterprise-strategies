908f9a11a94732dd6f402928a94adb021bc1e7a9
emmaherrero <emmaherrero@ufl.edu>
2025-09-16 23:27:48 -0400

    fix: resolve App.js conflict; align image import variable name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import gator from './gator.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={gator} className="App-logo" alt="gator" />
         <p>
           Hello World! This is our team project.
         </p>	
